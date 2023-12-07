f3cab4571b8f0b7f90ad82a2bc0c4e92e4bbced3
Derek Sonnenberg <derek@ardrive.io>
2023-12-07 11:09:34 -0600

    chore(gift app): use dev payment PE-5161

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const paymentServiceUrl =
   import.meta.env.PROD === true
-    ? "https://payment.ardrive.io"
+    ? "https://payment.ardrive.dev" // TODO: change to https://payment.ardrive.io when gifting is in prod
     : "http://localhost:3000";
 export const termsOfServiceUrl = "https://ardrive.io/tos-and-privacy/";
 export const defaultUSDAmount = 10.0;
