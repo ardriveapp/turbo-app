a1521d2cfeb5dd417badc2d27abe182a5a083966
Derek Sonnenberg <derek@ardrive.io>
2023-12-07 11:52:55 -0600

    chore: deploy latest PE-5161

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GiftForm } from "./GiftForm";
 import { RouterPage } from "./components/RouterPage";
 import { useErrorMessage } from "./hooks/useErrorMessage";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [errorMessage, setErrorMessage] = useErrorMessage();
