c6bd0a4574720a2cf4a144d4d19c9c6097407acb
Derek Sonnenberg <derek@ardrive.io>
2023-12-07 19:49:17 -0600

    refactor: remove error logic from router PE-5161

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,36 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GiftForm } from "./GiftForm";
-import { RouterPage } from "./components/RouterPage";
-import { useErrorMessage } from "./hooks/useErrorMessage";
+import { Page } from "./components/RouterPage";
 
 export function Router() {
-  const [errorMessage, setErrorMessage] = useErrorMessage();
-
   return (
     <BrowserRouter basename={`${import.meta.env.BASE_URL}`}>
       <Routes>
         <Route
           path="/gift"
           element={
-            <RouterPage
-              page={<GiftForm errorCallback={setErrorMessage} />}
-              errorMessage={errorMessage}
+            <Page
+              page={(errorCallback) => (
+                <GiftForm errorCallback={errorCallback} />
+              )}
             />
           }
         />
-        <Route
-          path="/redeem"
-          element={
-            <RouterPage page={<p>TODO</p>} errorMessage={errorMessage} />
-          }
-        />
+        <Route path="/redeem" element={<Page page={() => <p>TODO</p>} />} />
         <Route
           path="/"
           element={
             /* Put gift element at baseUrl for now */
-            <RouterPage
-              page={<GiftForm errorCallback={setErrorMessage} />}
-              errorMessage={errorMessage}
+            <Page
+              page={(errorCallback) => (
+                <GiftForm errorCallback={errorCallback} />
+              )}
             />
           }
         />
--- src/components/RouterPage.tsx
+++ src/components/RouterPage.tsx
@@ -1,17 +1,19 @@
 import { termsOfServiceUrl } from "../constants";
+import { useErrorMessage } from "../hooks/useErrorMessage";
 import { LogoHeader } from "./LogoHeader";
 import "./RouterPage.css";
 
-interface RouterPageProps {
-  page: JSX.Element;
-  errorMessage?: string;
+interface PageProps {
+  page: (errorCallback: (message: string) => void) => JSX.Element;
 }
 
-export function RouterPage({ page, errorMessage }: RouterPageProps) {
+export function Page({ page }: PageProps) {
+  const [errorMessage, errorCallback] = useErrorMessage();
+
   return (
     <>
       <LogoHeader errorMessage={errorMessage} />
-      {page}
+      {page(errorCallback)}
       <span id="version-footer">
         <a href={termsOfServiceUrl}>Terms</a> | v
         {import.meta.env.PACKAGE_VERSION}
