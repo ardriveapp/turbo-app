f7b5ec57c64ed240a91a2fb27473e6b7c1bb7665
Derek Sonnenberg <derek@ardrive.io>
2023-12-13 12:16:57 -0600

    style: update spacing PE-5161

---
+++ src/pages/GiftPage.content.ts
@@ -0,0 +1,9 @@
+export const GiftPageContent = {
+  header: "Gift Storage Credits",
+  headerParagraph:
+    "Credits can be redeemed for a secure and permanent place to store precious memories such as photos, videos, docs and more.",
+  recipientParagraph:
+    "Your recipient will receive an email with instructions on how to redeem and use their gift of permanent storage.",
+  recipientParagraph2:
+    "If you'd prefer them to receive the email at another time, please enter your own email address and plan to forward the email when you'd like the recipient to receive your gift.",
+};
